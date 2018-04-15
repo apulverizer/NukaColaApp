f6e153ebcc76809d27150119abe87566e45199cd
Aaron Pulver <apulver@esri.com>
2018-04-14 21:40:47 -0400

    wip

--- app/components/led-card.js
+++ app/components/led-card.js
@@ -8,7 +8,7 @@ export default Component.extend({
   colorStyle: Ember.computed('led.color', function(){
     return Ember.String.htmlSafe('color:' + this.get('led.color'));
   }),
-
+  paletteString: '[["e57373", "f44336", "d32f2f", "b71c1c"], ["ba68c8", "9c27b0", "7b1fa2", "4a148c"], ["7986cb", "3f51b5", "303f9f", "1a237e"], ["64b5f6", "2196f3", "1976d2", "0d47a1"], ["4dd0e1", "00bcd4", "0097a7", "006064"], ["81c784", "4caf50", "388e3c", "1b5e20"], ["dce775", "cddc39", "afb42b", "827717"], ["fff176", "ffeb3b", "fbc02d", "f57f17"], ["ffb74d", "ff9800", "f57c00", "e65100"], ["a1887f", "795548", "5d4037", "3e2723"], ["e0e0e0", "9e9e9e", "616161", "000000"]]',
   actions: {
     stateChanged(){
       this.get('store').findRecord('led', this.get('led.id')).then(function(led){
